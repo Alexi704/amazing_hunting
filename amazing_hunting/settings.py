77fa375eb21c9849e75c126fc4bc5aa43950e198
Tuman8 <i-a-l-e-x@yandex.ru>
2022-07-30 16:48:06 +0700

    30.1.2 создаем модель пользьвателей

--- amazing_hunting/settings.py
+++ amazing_hunting/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'authentication',
     'vacancies',
     'companies',
 ]
@@ -150,3 +151,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = 'authentication.User'
